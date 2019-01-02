f399b02aba09798475b9b0cb4c12383009e08423
Luke Samad <lhsamad@gmail.com>
2019-01-01 23:58:25 -0500

    renaming

--- src/main/java/com/alif/bootreact/BootReactApplication.java
+++ src/main/java/com/alif/bootreact/BootReactApplication.java
@@ -38,7 +38,6 @@ public class BootReactApplication {
     @Bean
     CommandLineRunner runner() {
         return args -> {
-            /*
             userRepository.save(new User("user", "$2a$04$1.YhMIgNX/8TkCKGFUONWO1waedKhQ5KrnB30fl0Q01QKqmzLf.Zi", "USER"));
             userRepository.save(new User( "admin", "$2a$04$KNLUwOWHVQZVpXyMBNc7JOzbLiBjb9Tk9bP7KNcPI12ICuvzXQQKG", "ADMIN"));
 
@@ -50,7 +49,7 @@ public class BootReactApplication {
 
             carRepository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000, owner1));
             carRepository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000, owner2));
-            carRepository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000, owner2));*/
+            carRepository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000, owner2));
 
         };
     }
